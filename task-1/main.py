225f1afe01623cf01bab9ea69529539f51ffe9d9
Yahor Kuchar <egorkuchar@gmail.com>
2022-02-21 13:18:58 +0300

    add __name__=main

--- task-1/main.py
+++ task-1/main.py
@@ -1,30 +1,6 @@
 import argparse
 from text_parser import TextParser
-
-
-def positive_integer(value: int) -> int:
-    ivalue = int(value)
-
-    if ivalue < 1:
-        raise argparse.ArgumentTypeError(
-            f"{value} is invalid. Value must be positive integer.")
-
-    return ivalue
-
-
-def init_args_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--n", type=positive_integer, help="'n' for n-gram. n > 0. Default: %(default)s", default=4)
-
-    parser.add_argument(
-        "--k", type=positive_integer,  help="top sort of k n-grams. k > 0. Default: %(default)s", default=10)
-
-    parser.add_argument("--input", type=str,
-                        help="input file name. Default: %(default)s", default="input.txt")
-
-    return parser
-
+from args_parser import init_args_parser
 
 def main():
     parser = init_args_parser()
@@ -34,4 +10,5 @@ def main():
     tp.print_statistics()
 
 
-main()
+if __name__ == '__main__':
+    main()
