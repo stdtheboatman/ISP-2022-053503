b399088fe8389c0ee74014a2e166bcac382bde04
Yahor Kuchar <egorkuchar@gmail.com>
2022-02-21 12:46:24 +0300

    fixes

--- task-1/main.py
+++ task-1/main.py
@@ -5,10 +5,10 @@ from text_parser import TextParser
 def init_args_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--n", type=int, help="'n' for n-gram. Default: %(default)s", default=4)
+        "--n", type=int, help="'n' for n-gram. Default: %(default)s", default=4, choices=range(1, 100))
 
     parser.add_argument(
-        "--k", type=int,  help="top sort of k n-grams. Default: %(default)s", default=10)
+        "--k", type=int,  help="top sort of k n-grams. Default: %(default)s", default=10, choices=range(1, 10**6))
 
     parser.add_argument("--input", type=str,
                         help="input file name. Default: %(default)s", default="input.txt")
