61eb9f88e6d9ac8fc223424a5b5a385a2dfd6f59
stdtheboatman <egorkuchar@gmail.com>
2022-05-28 15:26:03 +0300

    refactoring

--- task-3/backend/backend/urls.py
+++ task-3/backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""pet_project URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,15 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from api import views
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('v1/ping', include('rest_framework.urls', namespace='rest_framework'))
-    path('v1/ping', views.PingView.as_view())
 ]
-
-
