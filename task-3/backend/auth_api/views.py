d5bb1d8d4361e1eeb6b3df9f277ad06a6e09f9d6
stdtheboatman <egorkuchar@gmail.com>
2022-05-28 16:46:44 +0300

    add auth_api

--- task-3/backend/auth_api/views.py
+++ task-3/backend/auth_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        'api/token/refresh'      
+    ]
+    
+    return Response(routes)
